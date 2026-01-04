edd8894a30411693699484d61c57d7ff1bc5af81
ovesh boss <oveshboss.08@gmail.com>
2026-01-04 12:05:10 +0530

    Update config.py

--- config.py
+++ config.py
@@ -13,7 +13,7 @@ import os
 API_ID = int(os.getenv("API_ID", 0))
 API_HASH = os.getenv("API_HASH", "")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "")
-MONGO_URI = os.getenv("MONGO_URI", "")
+MONGO_URI = os.getenv("MONGO_URI", "mongodb+srv://Ovesh:ovesh.boss@ovesh.95jpp8g.mongodb.net/?retryWrites=true&w=majority&appName=Ovesh")
 DB_NAME = os.getenv("DB_NAME", "Cluster0")
 
 # Owner and bot details
